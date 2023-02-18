ed4b3c16be2fe13d3846e618bf79605ffd5179aa
oliverwang15 <oliverwang15@qq.com>
2023-02-18 15:55:11 +0800

    updates on chatgpt-trading

--- finnlp/data_sources/news/_base.py
+++ finnlp/data_sources/news/_base.py
@@ -1,10 +1,10 @@
 class News_Downloader:
-    def __init__(self,args):
+
+    def __init__(self, args):
         pass
 
-    def download_news(self,start_date,end_date,stock):
+    def download_news(self, start_date, end_date, stock):
         pass
-
 
     def clean_data(self):
         pass
